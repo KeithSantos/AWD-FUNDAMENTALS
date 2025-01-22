31bc9b5c75abb4b2773e1ceec306b7c136221d01
Keith Santos <iamkeithsantos@gmail.com>
2025-01-23 00:12:49 +0800

    Refactor(landing page and 2nd page partially done)

---
+++ pages/page1/assets/js/scripts.js
@@ -0,0 +1,10 @@
+let message = "Welcome to the Fundamentals of Programming!";
+const year = 2024;
+var greeting = "Hello, World!";
+
+console.log(message);
+console.log("Current Year: " + year);
+console.log(greeting);
+
+message = "Exploring Programming Concepts!";
+console.log(message);
